36f8b3e7b6c9f4178f01fe561b1a72ab99afd4b6
Ondra Kavan <ondrasik007@gmail.com>
2022-10-08 14:24:50 +0100

    Cleanup

--- Scripts/File.cs
+++ Scripts/File.cs
@@ -53,15 +53,5 @@ namespace PeacefulBeast.ModLib
 
             return texture;
         }
-        /// <summary>
-        /// Extension function to convert a Texture2D into a Sprite
-        /// </summary>
-        /// <param name="size">Size of the image</param>
-        public static Sprite ToSprite(this Texture2D texture, ushort size)
-        {
-            var sprite = Sprite.Create(texture, new Rect(0, 0, size, size), new Vector2(0.5f, 0.5f), size);
-
-            return sprite;
-        }
     }
 }
