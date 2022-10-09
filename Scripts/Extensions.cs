c9ae9b441bbda993ae4296acd7cb56fe98ac636a
Ondra Kavan <ondrasik007@gmail.com>
2022-10-09 16:58:02 +0100

    Fixed extensions.cs
    The class was set as internal instead of public.

--- Scripts/Extensions.cs
+++ Scripts/Extensions.cs
@@ -1,8 +1,10 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
 
 namespace PeacefulBeast.ModLib
 {
-    internal static class Extensions
+    public static class Extensions
     {
         /// <summary>
         /// Extension function to convert a Texture2D into a Sprite
@@ -13,6 +15,23 @@ namespace PeacefulBeast.ModLib
             var sprite = Sprite.Create(texture, new Rect(0, 0, size, size), new Vector2(0.5f, 0.5f), size);
 
             return sprite;
+        }
+
+        /// <summary>
+        /// Return positions of all the 8 neighbours.
+        /// </summary>
+        public static List<Vector2Int> Neighbours(this Vector2Int vector)
+        {
+            var result = new List<Vector2Int>();
+            for (int y = 1; y > -1; y--)
+            {
+                for (int x = -1; x < 1; x++)
+                {
+                    if (x == 0 && y == 0) continue;
+                    result.Add(new Vector2Int(x, y));
+                }
+            }
+            return result;
         }
     }
 }
